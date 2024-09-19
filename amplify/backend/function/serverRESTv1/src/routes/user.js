07c47a1f52022bc7c34a1fa02690d66fe639dd9b
will1225 <ixus_tse@hotmail.com>
2024-09-19 16:20:44 -0400

    Add FitnessUtils class and API route

--- amplify/backend/function/serverRESTv1/src/routes/user.js
+++ amplify/backend/function/serverRESTv1/src/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../controller/User');
 const Profile = require("../controller/Profile");
+const FitnessUtils = require('../controller/FitnessUtils');
 
 router.post("/signin", User.login);
 router.post("/signup", User.signup);
@@ -9,5 +10,6 @@ router.get("/verify/:otp", User.verify);
 router.post("/forgot", User.forgotPassword);
 router.post("/signin/provider", User.loginWithProvider);
 router.post("/profile/enter", Profile.saveProfile);
+router.get("/result/:bodyMeasurementId", FitnessUtils.getFitnessResult);
 
 module.exports = router;
