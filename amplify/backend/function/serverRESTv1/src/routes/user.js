403e8c62863376c9e1f114809e942f8cb0ed4584
will1225 <ixus_tse@hotmail.com>
2024-09-27 13:37:58 -0400

    Revert "Added User Global State API and route"
    This reverts commit 0a88bf199fed2822337782a6c90b0877c544c6cf.

--- amplify/backend/function/serverRESTv1/src/routes/user.js
+++ amplify/backend/function/serverRESTv1/src/routes/user.js
@@ -10,7 +10,6 @@ router.get("/verify/:otp", User.verify);
 router.post("/forgot", User.forgotPassword);
 router.post("/signin/provider", User.loginWithProvider);
 router.post("/profile/enter", Authorization.verifyToken, Profile.saveProfile);
-router.get("/globalState", User.globalState);
 
 //Verify if the users token is valid or if the user is quthorized
 router.get("/authenticate", Authorization.verifyToken, (req, res) => res.status(200).json({status: true, message: "Authorized"}));
